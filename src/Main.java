7c79ccfc0080a386cb8292dc298f5221fe96d96d
vergik <mihailovpeter@gmail.com>
2020-09-30 02:49:07 +0300

    -

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.addAll(Arrays.asList("ab", "bc","_sfa", "zdwq"));
+        System.out.println(list);
+    }
+}
